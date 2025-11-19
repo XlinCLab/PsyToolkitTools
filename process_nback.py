805449730d25659cb90eb17b350f8cedb3c9e94f
Philip Georgis <philip.georgis@gmail.com>
2025-11-19 16:00:22 +0100

    Extract reaction times for all conditions for nback task

--- process_nback.py
+++ process_nback.py
@@ -31,6 +31,18 @@ def main(results_file):
     n_correct_nonmatching = sum(nonmatching_condition_results["score"])
     n_incorrect_nonmatching = len(nonmatching_condition_results[nonmatching_condition_results["score"] == 0])
 
+    # Extract reaction time per condition (matching/non-matching) and status (correct/incorrect)
+    rt_matching_overall = mean(matching_condition_results["reaction_time"])
+    rt_nonmatching_overall = mean(nonmatching_condition_results["reaction_time"])
+    matching_correct = matching_condition_results[matching_condition_results["score"] == 1]
+    matching_incorrect = matching_condition_results[matching_condition_results["score"] == 0]
+    rt_matching_correct = mean(matching_correct["reaction_time"]) if len(matching_correct) > 0 else pd.NA
+    rt_matching_incorrect = mean(matching_incorrect["reaction_time"]) if len(matching_incorrect) > 0 else pd.NA
+    nonmatching_correct = nonmatching_condition_results[nonmatching_condition_results["score"] == 1]
+    nonmatching_incorrect = nonmatching_condition_results[nonmatching_condition_results["score"] == 0]
+    rt_nonmatching_correct = mean(nonmatching_correct["reaction_time"]) if len(nonmatching_correct) > 0 else pd.NA
+    rt_nonmatching_incorrect = mean(nonmatching_incorrect["reaction_time"]) if len(nonmatching_incorrect) > 0 else pd.NA
+
     processed_results = {
         "overall_accuracy": overall_accuracy,
         "accuracy_matching": accuracy_matching,
@@ -39,5 +51,11 @@ def main(results_file):
         "n_incorrect_matching": n_incorrect_matching,
         "n_correct_nonmatching": n_correct_nonmatching,
         "n_incorrect_nonmatching": n_incorrect_nonmatching,
+        "rt_matching_overall": rt_matching_overall,
+        "rt_nonmatching_overall": rt_nonmatching_overall,
+        "rt_matching_correct": rt_matching_correct,
+        "rt_matching_incorrect": rt_matching_incorrect,
+        "rt_nonmatching_correct": rt_nonmatching_correct,
+        "rt_nonmatching_incorrect": rt_nonmatching_incorrect,
     }
     return processed_results
